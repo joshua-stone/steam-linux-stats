1796e34bf2931f737eb2b10a51b714524428bb8c
Joshua Stone <joshua.gage.stone@gmail.com>
2016-11-03 00:04:41 -0400

    Create support status Enum, with color codes for prettier printing

--- steam-linux-stats.py
+++ steam-linux-stats.py
@@ -1,5 +1,14 @@
 #!/usr/bin/env python3
 
+from enum import Enum
+
+steam_store = 'http://store.steampowered.com/'
+
+class status(Enum):
+    supported = '\033[92mSUPPORTED\033[0m'
+    unsupported = '\033[91mUNSUPPORTED\033[0m'
+    unknown = 'UNKNOWN'
+
 if __name__ == '__main__':
     main()
 
