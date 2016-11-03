2f8271d82bb829df793be5c3d55834ffb8c9ebf1
Joshua Stone <joshua.gage.stone@gmail.com>
2016-11-03 00:10:40 -0400

    Create is_supported() to parse Steam store JSON and output three possible support status codes

--- steam-linux-stats.py
+++ steam-linux-stats.py
@@ -9,6 +9,24 @@ class status(Enum):
     unsupported = '\033[91mUNSUPPORTED\033[0m'
     unknown = 'UNKNOWN'
 
+def is_supported(store_url):
+    if store_url.startswith('{}app/'.format(steam_store)):
+        store_id = store_url.split('/')[-2]
+        api_url = '{}api/appdetails?appids={}'.format(steam_store, store_id)
+
+        json = loads(urlopen(api_url).read().decode())
+        linux_support = json[store_id]['data']['platforms']['linux']
+
+        if linux_support:
+            support_status = status.supported
+        else:
+            support_status = status.unsupported
+    else:
+        support_status = status.unknown
+
+    return support_status
+
+
 if __name__ == '__main__':
     main()
 
