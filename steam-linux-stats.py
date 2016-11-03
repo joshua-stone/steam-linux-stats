5bf2f65b21e4cb97e6d9f52c3ed53776cb1f89fa
Joshua Stone <joshua.gage.stone@gmail.com>
2016-11-03 00:44:54 -0400

    Create top_games() generator to scrape Steam's top 100 games and output info for each title

--- steam-linux-stats.py
+++ steam-linux-stats.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
+from json import loads
 from enum import Enum
 
 steam_store = 'http://store.steampowered.com/'
@@ -26,6 +29,18 @@ def is_supported(store_url):
 
     return support_status
 
+def top_games():
+    html = urlopen('{}stats/'.format(steam_store))
+    bs = BeautifulSoup(html.read(), 'html.parser')
+    top_games = bs.findAll('tr', class_='player_count_row')
+
+    for game in [game.contents for game in top_games]:
+        current, peak = game[1].contents[1].contents[0], game[3].contents[1].contents[0]
+        title, store_url = game[7].contents[1].get_text(), game[7].contents[1].get('href')
+
+        support_status = is_supported(store_url)
+
+        yield (current, peak, title, support_status.value)
 
 if __name__ == '__main__':
     main()
